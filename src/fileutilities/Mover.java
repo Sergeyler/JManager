135807942fce48681937d3f790768647c6579c9a
SergeyLebidko <it.slavtub@mail.ru>
2017-04-28 17:19:14 +0300

    Завершено создание диалога конфликта файлов

--- src/fileutilities/Mover.java
+++ src/fileutilities/Mover.java
@@ -1,7 +1,11 @@
 package fileutilities;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.nio.*;
 import javax.swing.*;
@@ -26,13 +30,19 @@ public class Mover extends SwingWorker<Void, Void>{
     //Список объектов из source, которые после выполнения процедуры копирования/перемещения надо сделать выделенными в папке target
     private File[] allocateList;
 
-    //Список элементов пользовательского интерфейса, необходимых для работы процедуры
-
+    //Список полей, необходимых для построения окна копирования
     private static JFrame copyFrame=null;
-    private static final JLabel label=new JLabel("");
+    private static final JLabel nameCopyFile=new JLabel("");
     private static final JProgressBar progressBar=new JProgressBar();
     private static String titleFrame="";
     private static String labelPrefix="";
+
+    //Список полей, необходимых для отображения диалога конфликта файлов
+    private static JDialog conflictDialog=null;
+    private static JLabel nameConflictFile=new JLabel("Файл: ");
+    private static JComboBox<String> optForAction=new JComboBox<>(new String[]{"Не копировать этот файл", "Заменить файл в папке назначения", "Скопировать, но сохранить оба файла"});
+    private static JCheckBox defaultActBox=new JCheckBox("Всегда выполнять выбранное действие", true);
+    private static JButton okBtn=new JButton("OK");
 
     public Mover (File sourceFolder, File[] source, File targetFolder, int copyOption){
         this.sourceFolder=sourceFolder;
@@ -49,6 +59,8 @@ public class Mover extends SwingWorker<Void, Void>{
             titleFrame="Перемещение";
             labelPrefix="Перемещаю: ";
         }
+
+        //Создаем диалог копирования
         if(copyFrame==null){
             int frameWidth=600;
             int frameHeight=100;
@@ -62,7 +74,7 @@ public class Mover extends SwingWorker<Void, Void>{
             Box p0=Box.createVerticalBox();
             Box p1=Box.createHorizontalBox();
             p0.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-            p1.add(label);
+            p1.add(nameCopyFile);
             p1.add(Box.createHorizontalGlue());
             p0.add(p1);
             p0.add(Box.createVerticalGlue());
@@ -71,10 +83,65 @@ public class Mover extends SwingWorker<Void, Void>{
             copyFrame.add(p0);
         }
         copyFrame.setTitle(titleFrame);
-        label.setText(labelPrefix);        //Предельная длина выводимого пути - 139 символов
+        nameCopyFile.setText(labelPrefix);        //Предельная длина выводимого пути - 139 символов
         progressBar.setMinimum(0);
         progressBar.setValue(0);
         copyFrame.setVisible(true);
+
+        //Создаем диалог, который будет отображаться в случае конфликта имен файлов
+        if(conflictDialog==null){
+            int dialogWidth=300;
+            int dialogHeight=200;
+            conflictDialog=new JDialog(copyFrame, true);
+            conflictDialog.setTitle("Внимание");
+            conflictDialog.setSize(new Dimension(dialogWidth, dialogHeight));
+            conflictDialog.setResizable(false);
+            conflictDialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+            int xPos=Toolkit.getDefaultToolkit().getScreenSize().width/2-dialogWidth/2;
+            int yPos=Toolkit.getDefaultToolkit().getScreenSize().height/2-dialogHeight/2;
+            conflictDialog.setLocation(xPos, yPos);
+
+            Box p0=Box.createVerticalBox();
+            p0.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+            Box p1=Box.createHorizontalBox();
+            p1.add(nameConflictFile);
+            p1.add(Box.createHorizontalGlue());
+            p0.add(p1);
+
+            p0.add(Box.createVerticalGlue());
+
+            Box p2=Box.createHorizontalBox();
+            p2.add(new JLabel("Выберите действие:"));
+            p2.add(Box.createHorizontalGlue());
+            p0.add(p2);
+
+            Box p3=Box.createHorizontalBox();
+            optForAction.setMaximumSize(new Dimension(dialogWidth-20, 20));
+            p3.add(optForAction);
+            p0.add(p3);
+
+            p0.add(Box.createVerticalStrut(10));
+
+            Box p4=Box.createHorizontalBox();
+            p4.add(defaultActBox);
+            p4.add(Box.createHorizontalGlue());
+            p0.add(p4);
+
+            Box p5=Box.createHorizontalBox();
+            p5.add(Box.createHorizontalGlue());
+            p5.add(okBtn);
+            p0.add(p5);
+
+            conflictDialog.setContentPane(p0);
+            okBtn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    conflictDialog.setVisible(false);
+                }
+            });
+        }
+
     }
 
     @Override
@@ -107,7 +174,10 @@ public class Mover extends SwingWorker<Void, Void>{
 
         for(int i=0;i<101;i++){
             progressBar.setValue(i);
-            Thread.sleep(50);
+            Thread.sleep(25);
+            if(i==50){
+                conflictDialog.setVisible(true);
+            }
         }
 
         copyFrame.setVisible(false);
